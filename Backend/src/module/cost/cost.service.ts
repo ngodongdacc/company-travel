f65914aaa36ae3322b9100c5645b195418dbb62a
Dong Ngo <ngodongdac3005@gmail.com>
2023-04-21 00:59:58 +0700

    cost: cost calculation

--- Backend/src/module/cost/cost.service.ts
+++ Backend/src/module/cost/cost.service.ts
@@ -2,16 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { CompanyService } from '../company/company.service';
 import { TravelService } from '../travel/travel.service';
 import { CompanyTreeCostDto } from './dto/companyTreeCost.dto';
-import { FindCompanyDto } from '../company/dto/find-company.dto';
 import { getCompanyTreeCost } from 'src/common/tree-cost';
 @Injectable()
 export class CostService {
   constructor(private readonly companyService: CompanyService, private readonly travelService: TravelService) {}
 
-  public async companyTreeCost(query: FindCompanyDto): Promise<[CompanyTreeCostDto[]]> {
-    query.limit = 0;
+  public async companyTreeCost(): Promise<[CompanyTreeCostDto[]]> {
     const [companies, travel] = await Promise.all([
-      this.companyService.getAllCompany(query).then((company) => company[0] || []),
+      this.companyService.getAllCompany({ limit: 0 }).then((company) => company[0] || []),
       this.travelService.getAllTravel({ limit: 0 }).then((travel) => travel[0] || []),
     ]);
     const data = getCompanyTreeCost('0', companies, travel);
